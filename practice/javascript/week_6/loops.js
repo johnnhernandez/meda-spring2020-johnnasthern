6712d64c88e7d3298fc00c9e930ae10455c695a6
Johnnasthern Baca-Hernandez <bacahernandezj@gmail.com>
2020-02-28 11:40:38 -0800

    added loops

---
+++ practice/javascript/week_6/loops.js
@@ -0,0 +1,27 @@
+console.log("Testing");
+
+var counter = 1;
+
+
+// A loop needs a computer so we can track how many times it hasa loop
+var counter = 0;
+
+// If counter is less than 10, then...
+while (counter<=10) {
+    // console.log(number);
+
+    //...console.log hello, and add a 1 to the counter.
+    console.log(counter)
+    console.log("hello")
+    counter = counter + 1;
+
+};
+
+// Do while loop " do {}  while ()";
+
+var counter2 = 0;
+
+do {
+    console.log("goodbye");
+    counter2 = counter2 + 1;
+} while (false);
