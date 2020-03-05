63790ed86dc1fab6811f2367a500c6931ab8e5be
Johnnasthern Baca-Hernandez <bacahernandezj@gmail.com>
2020-03-05 09:53:13 -0800

    revised pyramid builder

---
+++ practice/javascript/week_7/pyramidbuilder.js
@@ -0,0 +1,38 @@
+function pyramidBuilder(height) {
+
+    var pyramidHeight;
+
+    if (height == undefined) {
+        var randomNumber = (Math.random() * 20) + 1;
+        pyramidHeight = Math.floor(randomNumber);
+    } else {
+        pyramidHeight = height;
+    }
+
+    var pyramidRow = "";
+
+    for (var i = 0; i < pyramidHeight; i++) {
+        pyramidRow = pyramidRow + "**";
+        console.log(pyramidRow);
+    }
+
+    var pyramidArray = pyramidRow.split("");
+
+    for (var i = 0; i < pyramidHeight - 1; i++) {
+        pyramidArray.pop();
+        pyramidArray.pop();
+        pyramidRow = pyramidArray.join("");
+        console.log(pyramidRow);
+    }
+
+}
+
+
+pyramidBuilder(3);
+pyramidBuilder();
+pyramidBuilder(3);
+
+
+for (var i = 0; i < 100; i++) {
+    pyramidBuilder();
+}
